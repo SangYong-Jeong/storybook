d4429920b931b462c46016139d3c4234268d18e6
SangYong-Jeong <mmaf@naver.com>
2022-08-18 09:58:43 +0900

    storybook interaction test

--- src/stories/portfolio/portfolio-form/PfForm.jsx
+++ src/stories/portfolio/portfolio-form/PfForm.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { PfInput } from '../portfolio-input/PfInput';
 import './PfForm.scss';
 
-export const PfForm = ({ labels }) => {
+export const PfForm = ({ labels, onClickButton }) => {
+  const [isSubmit, setIsSubmit] = useState(false);
+  const onClickFormButton = () => {
+    onClickButton();
+    setIsSubmit(true);
+  };
   return (
-    <form className='pf-form'>
-      {labels.map(label => (<PfInput key={`input_${label}`} label={label} />))}
+    <form className="pf-form">
+      {labels.map((label) => (
+        <PfInput key={`input_${label}`} label={label} />
+      ))}
+      <button
+        type="button"
+        data-testid={'button'}
+        className="pf-form__button"
+        onClick={onClickFormButton}
+      >
+        Submit
+      </button>
+      {isSubmit ? (
+        <div className="pf-form__complete-text">
+          Everything is perfect, Your Message is sended! Have a good day!!
+        </div>
+      ) : null}
     </form>
-  )
+  );
 };
 
 PfForm.propTypes = {
   labels: PropTypes.array,
-}
+  onClickButton: PropTypes.func,
+};
--- src/stories/portfolio/portfolio-form/PfForm.stories.jsx
+++ src/stories/portfolio/portfolio-form/PfForm.stories.jsx
@@ -1,4 +1,5 @@
-import { screen, userEvent, fireEvent } from '@storybook/testing-library';
+import { screen, userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { PfForm } from './PfForm';
 
 export default {
@@ -15,24 +16,35 @@ export default {
 
 const Template = (args) => <PfForm {...args} />;
 
-export const FirstStory = Template.bind({});
-FirstStory.args = {
+export const EmptyForm = Template.bind({});
+EmptyForm.args = {
   labels: ['Name', 'Email', 'Message'],
+  onClickButton: () => {
+    window.alert('전송이 완료되었습니다.');
+  },
 };
-FirstStory.play = async () => {
-  const values = {
-    Name: 'Sanchez',
-    Email: 'Sanchez@gmail.com',
-    Message: 'Hello, World!',
-  };
-  for (let label of FirstStory.args.labels) {
-    await userEvent.type(screen.getByTestId(label), values[label]);
+
+export const FilledForm = Template.bind({});
+FilledForm.args = {
+  ...EmptyForm.args,
+};
+FilledForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  for (let label of EmptyForm.args.labels) {
+    await userEvent.type(canvas.getByTestId(label), label, { delay: 100 });
   }
+  await userEvent.click(canvas.getByTestId('button'));
+
+  await expect(
+    canvas.getByText(
+      'Everything is perfect, Your Message is sended! Have a good day!!'
+    )
+  ).toBeInTheDocument;
 };
 
 export const DelayedStory = Template.bind({});
 DelayedStory.args = {
-  ...FirstStory.args,
+  ...EmptyForm.args,
 };
 DelayedStory.play = async () => {
   const values = {
@@ -40,7 +52,7 @@ DelayedStory.play = async () => {
     Email: 'Sanchez@gmail.com',
     Message: 'Hello, World!',
   };
-  for (let label of FirstStory.args.labels) {
+  for (let label of EmptyForm.args.labels) {
     await userEvent.type(screen.getByTestId(label), values[label], {
       delay: 100,
     });
